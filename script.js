50cb5807b3a474b4782da61007eeb0c1f0d1f1b7
IvanGerman <ivgvi@mail.ru>
2020-08-02 19:19:27 +0200

    Commit4

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
-startCarProcess('car1');
-startCarProcess('car2');
-startCarProcess('car3');
+(function() {
+    var garage = document.getElementById('garage');
+
+    for(var i = 0; i < 77; i++) {
+        var div = document.createElement('div');
+        var id = 'car' + i;
+        div.id = id;
+        garage.appendChild(div);
+        startCarProcess(id);
+    };
+})();
+
+
